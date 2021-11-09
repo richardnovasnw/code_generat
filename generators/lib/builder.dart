1225ea80edd33f97e917c2df22bcda334102ef49
richard novas <richard.novas@nativewit.in>
2021-11-09 10:06:16 +0530

    Operators added

--- generators/lib/builder.dart
+++ generators/lib/builder.dart
@@ -3,7 +3,7 @@ import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'src/subclass_generator.dart';
-import 'src/sum_generator.dart';
+import 'src/operator_generator.dart';
 
 Builder generateSubclass(BuilderOptions options) =>
     SharedPartBuilder([SubclassGenerator()], 'subclass_generator');
